635d35f03a6d6760e3c8f433fa7d4e9210da588d
claraSchellembergAbelenda <cs314059@fi365.ort.edu.uy>
2025-02-20 00:14:25 -0300

    tabla de registros de actividad

---
+++ src/components/Dashboard/ListadoActividades/ActivityTable/ActivityRow/ActivityRow.jsx
@@ -0,0 +1,25 @@
+import { useDispatch } from "react-redux";
+import {onDeleteActivity} from "../../../../../app/slices/userSlice";
+
+const ActivityRow =({img, nombre, tiempo, fecha})=>{
+    const disptcher = useDispatch();
+    const _onDeleteActivity=()=>{
+        disptcher(onDeleteActivity(nombre));
+    };
+
+
+    return(
+        <tr>
+            <td>{img}</td>
+            <td>{nombre}</td>
+            <td>{tiempo}</td>
+            <td>{fecha}</td>
+            <td>
+                <button className="btn btn-danger" onClick={_onDeleteActivity}>
+                Delete
+                </button>
+            </td>
+        </tr>
+    );
+};
+export default ActivityRow;
---
+++ src/components/Dashboard/ListadoActividades/ActivityTable/ActivityTable.jsx
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+import ActivityRow from "./ActivityRow/ActivityRow";
+
+const ActivityTable = ()=>{
+    const activities = useSelector((state)=>state.userSlice.registros);
+
+    return(
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Actividad</th>
+                    <th>Tiempo</th>
+                    <th>Fecha</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {activities.map((activity)=>(
+                    <ActivityRow
+                        key={activity.id}
+                        actividad ={activity.nombre}
+                        tiempo ={activity.tiempo}
+                        fecha={activity.fecha}
+                    />
+                ))}
+            </tbody>
+        </table>
+    );
+};
+export default ActivityTable;
